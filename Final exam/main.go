c9d371ddbb4c55c6903916411ebf82645f3d41d7
Alexander Timoshenko <altimnlife@gmail.com>
2023-05-24 13:18:13 +0300

    added from Windows

---
+++ Final exam/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"flag"
+	"github.com/spf13/viper"
+	"log"
+	"main/include/server"
+	"main/package/simulator"
+)
+
+func main() {
+
+	if err := initConfig(); err != nil {
+		log.Println("Configuration file not loaded")
+	}
+
+	mode := flag.String("mode", "server", "a string")
+	addr := flag.String("addr", viper.GetString("server.addr"), "a string")
+	port := flag.String("port", viper.GetString("server.port"), "a string")
+	flag.Parse()
+
+	simUrl := viper.GetString("simulator.addr") + ":" + viper.GetString("simulator.port")
+	url := *addr + ":" + *port
+
+	switch {
+	case *mode == "server":
+		server.StartServer(url)
+	case *mode == "simulator":
+		simulator.Start(simUrl)
+	}
+}
+
+// initConfig() - initializes the configuration file in yaml format
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
+}
